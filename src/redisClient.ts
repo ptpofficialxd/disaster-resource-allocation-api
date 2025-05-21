cbda500b8059693a8a084393d713304967480b0f
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 19:35:37 +0700

    add NOAUTH

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -3,6 +3,7 @@ import Redis from "ioredis";
 const redisClient = new Redis({
   host: Bun.env.REDIS_HOST,
   port: Number(Bun.env.REDIS_PORT),
+  password: Bun.env.REDIS_PASSWORD
 });
 
 export default redisClient;
