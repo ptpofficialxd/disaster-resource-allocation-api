5ba54516a3bcb6031be468a1b124e4f315765b4e
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 20:40:49 +0700

    fix env

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,12 +1,12 @@
 import Redis from "ioredis";
 
-const port = Number(Bun.env.REDIS_PORT);
+const port = Number(process.env.REDIS_PORT);
 const useTls = port === 6380;
 
 const redisClient = new Redis({
-  host: Bun.env.REDIS_HOST,
+  host: process.env.REDIS_HOST,
   port: port,
-  password: Bun.env.REDIS_PASSWORD,
+  password: process.env.REDIS_PASSWORD,
   tls: useTls ? { checkServerIdentity: () => undefined } : undefined,
 });
 
