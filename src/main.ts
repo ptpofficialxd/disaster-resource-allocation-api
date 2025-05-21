a9cfa48f58dd207f65fc5791a6aadf86200f9f66
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 19:17:44 +0700

    update port connection

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import app from "./api";
 // Start the server
 Bun.serve({
   hostname: "0.0.0.0",
-  port: 3000,
+  port: Bun.env.PORT,
   fetch: app.fetch,
 });
 
--- src/redisClient.ts
+++ src/redisClient.ts
@@ -3,8 +3,6 @@ import Redis from "ioredis";
 const redisClient = new Redis({
   host: Bun.env.REDIS_HOST,
   port: Number(Bun.env.REDIS_PORT),
-  password: Bun.env.REDIS_PASSWORD,
-  tls: Bun.env.REDIS_PORT === "6380" ? { checkServerIdentity: () => undefined } : undefined
 });
 
 export default redisClient;
