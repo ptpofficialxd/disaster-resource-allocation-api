7d89f8ed759914bc94a44321b3763c1f5c49c2b7
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 22:30:11 +0700

    test uri connection

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,13 +1,13 @@
 import Redis from "ioredis";
 
-const redisUri = `rediss://:${encodeURIComponent(process.env.REDIS_PASSWORD || '')}@${process.env.REDIS_HOST}:6380`;
+const redisUri = `rediss://:Qqh8Gh7NRJVW7ArHbl79NWk7s3m4JftNoAzCaPG4ssE=@ptpofficialxd.redis.cache.windows.net:6380`;
 
 const redisClient = new Redis(redisUri, {
   tls: {
-    servername: process.env.REDIS_HOST,
-    rejectUnauthorized: false // ปิดการตรวจสอบ certificate ใน development
+    servername: 'ptpofficialxd.redis.cache.windows.net',
+    rejectUnauthorized: false
   },
-  enableAutoPipelining: true,
+  enableAutoPipelining: true
 });
 
 export default redisClient;
