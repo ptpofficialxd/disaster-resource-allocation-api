d05ef0172f1f714bf9030cbdd41d57d22e239fa9
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 15:01:10 +0700

    update to use redis cache for azure

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -3,6 +3,8 @@ import Redis from "ioredis";
 const redisClient = new Redis({
   host: Bun.env.REDIS_HOST,
   port: Number(Bun.env.REDIS_PORT),
+  password: Bun.env.REDIS_PASSWORD,
+  tls: Bun.env.REDIS_PORT === "6380" ? { checkServerIdentity: () => undefined } : undefined
 });
 
 export default redisClient;
