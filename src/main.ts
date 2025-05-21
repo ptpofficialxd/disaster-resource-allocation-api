e04d1eea7fb90fec1ee02cbf48ea1b2317ae28cb
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 23:30:52 +0700

    merge redis logic to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
+import Redis from "ioredis";
 import app from "./api";
 
-// Start the server
-Bun.serve({
-  hostname: "0.0.0.0",
-  port: Bun.env.PORT,
-  fetch: app.fetch,
+// Connect to Redis
+const redis = new Redis({
+  host: Bun.env.REDIS_HOST,
+  port: Number(Bun.env.REDIS_PORT),
+  password: Bun.env.REDIS_PASSWORD,
+  tls: Bun.env.REDIS_PORT === "6380" ? { checkServerIdentity: () => undefined } : undefined
 });
 
-console.log("Server is running on http://localhost:3000");
+// Successfully connected to Redis
+redis.on("connect", () => {
+  console.log("Connected to Azure Redis");
+});
+
+// Handle Redis connection errors
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
+
+// Start localhost server
+Bun.serve({
+  hostname: "0.0.0.0",
+  port: Number(Bun.env.PORT),
+  fetch: app.fetch
+});
+
+console.log(`Server is running on http://localhost:${Bun.env.PORT}`);
+
+export default redis;
