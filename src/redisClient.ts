f1b1e5a11dca58dab7c962c76a1dec2873effce0
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 20:54:08 +0700

    update redis and test workflows

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,13 +1,13 @@
 import Redis from "ioredis";
 
-const port = Number(process.env.REDIS_PORT);
-const useTls = port === 6380;
-
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: port,
-  password: process.env.REDIS_PASSWORD,
-  tls: useTls ? { checkServerIdentity: () => undefined } : undefined,
+  host: Bun.env.REDIS_HOST,
+  port: Number(Bun.env.REDIS_PORT),
+  password: Bun.env.REDIS_PASSWORD,
+  tls: {
+    servername: Bun.env.REDIS_HOST,
+    rejectUnauthorized: false,
+  },
 });
 
 export default redisClient;
