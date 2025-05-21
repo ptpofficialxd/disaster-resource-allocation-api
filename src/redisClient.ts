70c719152b4983f0c099e4c42eaaa158b721022e
ptpofficialxd <aekkungzaza@gmail.com>
2025-05-21 22:42:03 +0700

    update connection method

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const redisUri = `rediss://:Qqh8Gh7NRJVW7ArHbl79NWk7s3m4JftNoAzCaPG4ssE=@ptpofficialxd.redis.cache.windows.net:6380`;
+const redisUri = `rediss://:${encodeURIComponent(process.env.REDIS_PASSWORD || '')}@${process.env.REDIS_HOST}:6380`;
 
 const redisClient = new Redis(redisUri, {
   tls: {
